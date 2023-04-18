d48505be3760f5727b0a8d7c93e56e2270ec7709
Samuel Teixeira <steixeira@ua.pt>
2023-04-18 02:46:41 +0100

    Getting Sensor Data code started (Home.kt)

--- Clima/app/src/main/java/pt/ua/clima/Home.kt
+++ Clima/app/src/main/java/pt/ua/clima/Home.kt
@@ -1,11 +1,87 @@
 package pt.ua.clima
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Home : AppCompatActivity() {
+class Home : AppCompatActivity(), SensorEventListener {
+
+    private lateinit var sensorManager: SensorManager
+    private var temperatureSensor: Sensor? = null
+    private var humiditySensor: Sensor? = null
+    private var pressureSensor: Sensor? = null
+    private var lightSensor: Sensor? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        temperatureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE)
+        humiditySensor = sensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY)
+        pressureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE)
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+        
+    }
+    override fun onResume() {
+        super.onResume()
+
+        temperatureSensor?.let {
+            sensorManager.registerListener(
+                this,
+                it,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
+        }
+        humiditySensor?.let {
+            sensorManager.registerListener(
+                this,
+                it,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
+        }
+        pressureSensor?.let {
+            sensorManager.registerListener(
+                this,
+                it,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
+        }
+        lightSensor?.let {
+            sensorManager.registerListener(
+                this,
+                it,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        when (event.sensor.type) {
+            Sensor.TYPE_AMBIENT_TEMPERATURE -> {
+                val temperature = event.values[0]
+            }
+            Sensor.TYPE_RELATIVE_HUMIDITY -> {
+                val humidity = event.values[0]
+            }
+            Sensor.TYPE_PRESSURE -> {
+                val pressure = event.values[0]
+            }
+            Sensor.TYPE_LIGHT -> {
+                val luminosity = event.values[0]
+            }
+        }
     }
 }
